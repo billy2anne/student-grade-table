b1dec7c49fe4e4567d2714a3b7a600e1c9f611e9
Kimberly Roque <kimroque@gmail.com>
2020-05-06 16:42:59 -0700

    declared App class and added methods to start app, get grades, and handle success/error responses

--- features/components/app.js
+++ features/components/app.js
@@ -1,12 +1,31 @@
 class App {
   constructor(){
-
+    this.handleGetGradesError =  this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
   }
 
+  handleGetGradesError(error){
+    console.error(error);
+  }
 
+  handleGetGradesSuccess(grades) {
+    console.log(grades);
+  }
 
+  getGrades(){
+    debugger;
+    $.ajax({
+      type: "GET",
+      url:"https://sgt.lfzprototypes.com/api/grades",
+      headers:{
+        "X-Access-Token": "3A1U0wCu"
+      },
+      success: this.handleGetGradesSuccess,
+      error: this.handleGetGradesError
+    })
+  }
 
-
-
-
+  start(){
+    this.getGrades();
+  }
 }
