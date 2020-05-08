8428b46834614289b48953437fc7110c67151739
Kimberly Roque <kimroque@gmail.com>
2020-05-08 13:35:30 -0700

    stored header element in header variable and instantiated pageHeader in main.js

--- features/main.js
+++ features/main.js
@@ -1,4 +1,9 @@
 var table = document.querySelector('table');
+var header = document.querySelector('header');
+
 var gradeTable = new GradeTable(table);
+var testPageHeader = new PageHeader(header);
 var testApp =  new App(gradeTable);
+
 testApp.start();
+testPageHeader.updateAverage(20);
