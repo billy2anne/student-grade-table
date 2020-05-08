3cf0f60356cf4b56d6cb635756ac1f72a812aa70
Kimberly Roque <kimroque@gmail.com>
2020-05-07 20:05:41 -0700

    completedview all grades feature

--- features/components/grade-table.js
+++ features/components/grade-table.js
@@ -6,10 +6,12 @@ class GradeTable {
   updateGrades(grades){
     var tBody = this.tableElement.querySelector('tbody');
     for(var i = 0; i < grades.length; i++){
+    //table data
       var grade = grades[i].grade;
       var name = grades[i].name;
       var course = grades[i].course;
 
+      //create table data and modify text content to show student data
       var row = document.createElement('tr');
       var studentName = document.createElement('td');
       studentName.textContent = name;
@@ -18,6 +20,7 @@ class GradeTable {
       var studentGrade = document.createElement('td');
       studentGrade.textContent = grade;
 
+      //append new table data to new row and then append row to table body
       row.append(studentName, studentCourse, studentGrade);
       tBody.appendChild(row);
     }
