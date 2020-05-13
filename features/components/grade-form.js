7497ec7fee2a83a30ef25324d9101abfcccbb88f
Kimberly Roque <kimroque@gmail.com>
2020-05-12 22:07:14 -0700

    name, course, and grade variables declared to store formData values

--- features/components/grade-form.js
+++ features/components/grade-form.js
@@ -1,6 +1,5 @@
 class GradeForm{
   constructor(formElement){
-    debugger;
     this.formElement = formElement;
     this.handleSubmit = this.handleSubmit.bind(this);
     this.formElement.onsubmit = this.handleSubmit;
@@ -13,5 +12,10 @@ class GradeForm{
   handleSubmit(event){
     event.preventDefault();
     var formData = new FormData(event.target);
+    var name = formData.get("name");
+    var course = formData.get("course");
+    var grade = formData.get("grade");
+    this.createGrade(name, course, grade);
+    event.target.reset();
   }
 }
